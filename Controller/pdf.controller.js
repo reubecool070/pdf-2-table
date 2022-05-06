333c85bae4d44d3a29034949dcf5475d776bed25
reube <reubenthapa070@gmail.com>
2022-05-06 11:16:45 +0545

    [fix] check

--- Controller/pdf.controller.js
+++ Controller/pdf.controller.js
@@ -21,12 +21,12 @@ const pythonPromise = (url) =>{
       url,
     ]);
    py.stdout.on('data', function(data) {
+     console.log(typeof data);
      if (data) {
       data1 = data.toString()
+      console.log("data============", data1);
     }
-    console.log(typeof data1);
     //  data1 =  data.toString()
-    console.log("data============", data1);
    })
    py.on('close', (code) => {
      console.log("closed");
