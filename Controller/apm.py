aca90c2c2f97fb55db7d0323908d2768920982af
reube <reubenthapa070@gmail.com>
2022-06-08 10:08:20 +0545

    data set added and overfitting api started

--- Controller/apm.py
+++ Controller/apm.py
@@ -7,10 +7,27 @@ import pandas as pd
 import json
 from sys import exit
 from PIL import Image, ImageFilter, ImageEnhance
+import tensorflow as tf
+import pathlib
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
 # get relative path
 new_path = os.path.dirname(__file__)
 image_path = new_path + "/images/empty-6.jpg"
+
+# create a dataset
+batch_size = 32
+img_height = 50
+img_width = 150
+# getting model
+dataset_path = os.path.join(new_path, "APM_LABELS")
+data_dir = pathlib.Path(dataset_path)
+class_names = ["NA", "NO", "YES"]
+model = tf.keras.models.load_model('apm_model.h5')
+
 
 def sort_contours(cnts, method="left-to-right"):
     # initialize the reverse flag and sort index
@@ -35,9 +52,12 @@ def sort_contours(cnts, method="left-to-right"):
     # return the list of sorted contours and bounding boxes
     return (cnts, boundingBoxes)
 
+
 original_img = cv2.imread(image_path)
 
-#Functon for extracting the box
+# Functon for extracting the box
+
+
 def box_extraction(img_for_box_extraction_path, cropped_dir_path):
 
     print("Reading image..")
@@ -47,15 +67,15 @@ def box_extraction(img_for_box_extraction_path, cropped_dir_path):
     img_bin = 255-img_bin  # Invert the image
 
     print("Storing binary image to images/Image_bin.jpg..")
-    cv2.imwrite("images/empty-result.jpg",img_bin)
-    
+    cv2.imwrite("images/empty-result.jpg", img_bin)
 
     print("Applying Morphological Operations..")
     # Defining a kernel length
     kernel_length = np.array(img).shape[1]//40
-     
+
     # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
-    verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
+    verticle_kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (1, kernel_length))
     # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
     hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
     # A kernel of (3 X 3) ones.
@@ -64,25 +84,27 @@ def box_extraction(img_for_box_extraction_path, cropped_dir_path):
     # Morphological operation to detect verticle lines from an image
     img_temp1 = cv2.erode(img_bin, verticle_kernel, iterations=1)
     verticle_lines_img = cv2.dilate(img_temp1, verticle_kernel, iterations=5)
-    cv2.imwrite("images/verticle_lines.jpg",verticle_lines_img)
+    cv2.imwrite("images/verticle_lines.jpg", verticle_lines_img)
 
     # Morphological operation to detect horizontal lines from an image
     img_temp2 = cv2.erode(img_bin, hori_kernel, iterations=2)
     horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=2)
-    cv2.imwrite("images/horizontal_lines.jpg",horizontal_lines_img)
+    cv2.imwrite("images/horizontal_lines.jpg", horizontal_lines_img)
 
     # Weighting parameters, this will decide the quantity of an image to be added to make a new image.
     alpha = 0.5
     beta = 1.0 - alpha
     # This function helps to add two image with specific weight parameter to get a third image as summation of two image.
-    img_final_bin = cv2.addWeighted(verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
+    img_final_bin = cv2.addWeighted(
+        verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
     img_final_bin = cv2.erode(~img_final_bin, kernel, iterations=2)
-    (thresh, img_final_bin) = cv2.threshold(img_final_bin, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    (thresh, img_final_bin) = cv2.threshold(
+        img_final_bin, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
     # For Debugging
     # Enable this line to see verticle and horizontal lines in the image which is used to find boxes
     print("Binary image which only contains boxes: images/img_final_bin.jpg")
-    cv2.imwrite("images/img_final_bin.jpg",img_final_bin)
+    cv2.imwrite("images/img_final_bin.jpg", img_final_bin)
     # Find contours for image, which will detect all the boxes
     contours, hierarchy = cv2.findContours(
         img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -91,7 +113,7 @@ def box_extraction(img_for_box_extraction_path, cropped_dir_path):
 
     print("Output stored in Output directiory!")
 
-    idx = 400
+    idx = 0
     for c in contours[::-1]:
         # Returns the location and width,height for every contour
         x, y, w, h = cv2.boundingRect(c)
@@ -104,14 +126,23 @@ def box_extraction(img_for_box_extraction_path, cropped_dir_path):
             # resize image
             new_img = cv2.resize(new_img, (150, 40))
             cv2.imwrite(cropped_dir_path+str(idx) + '.png', new_img)
-
+            # load image to model
+            new_img = new_img.reshape(1, 40, 150, 3)
+            # tensor_img = tf.keras.utils.load_img(
+            #     new_img, target_size=(img_height, img_width)
+            # )
+            # img_array = tf.keras.utils.img_to_array(tensor_img)
+            # img_array = tf.expand_dims(img_array, 0) # Create a batch
+            predictions = model.predict(new_img)
+            print(predictions)
+            score = tf.nn.softmax(predictions[0])
+            print(idx, class_names[np.argmax(score)],100 * np.max(score))
 
     # For Debugging
     # Enable this line to see all contours.
-    cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
-    cv2.imwrite("./images/img_contour.jpg", img)
+    # cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
+    # cv2.imwrite("./images/img_contour.jpg", img)
 
 
-
-#Input image path and out folder
-box_extraction(image_path, "./cropped-6/")
+# Input image path and out folder
+box_extraction(image_path, "./cropped-0/")
---
+++ Controller/trainapm.py
@@ -0,0 +1,123 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+import pathlib
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+# getting relative path
+relative_path = os.path.dirname(__file__)
+dataset_path = os.path.join(relative_path, "APM_LABELS")
+
+# get the labels
+data_dir = pathlib.Path(dataset_path)
+image_count = len(list(data_dir.glob('*/*.png')))
+print(image_count)
+
+no = list(data_dir.glob('NO/*'))
+print("opening")
+
+im = PIL.Image.open(str(no[0]))
+print("open")
+im.show()
+
+# create a dataset
+batch_size = 32
+img_height = 40
+img_width = 150
+
+# train dataset
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+# validation dataset
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+#standardize the data
+normalization_layer = layers.Rescaling(1./255)
+# using that layer to convert to 0 ,1
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixel values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
+
+# create the model
+num_classes = len(class_names)
+model = Sequential([
+  # layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  # layers.Conv2D(16, 3, padding='same', activation='relu'),
+  # layers.MaxPooling2D(),
+  # layers.Conv2D(32, 3, padding='same', activation='relu'),
+  # layers.MaxPooling2D(),
+  # layers.Conv2D(64, 3, padding='same', activation='relu'),
+  # layers.MaxPooling2D(),
+  # layers.Flatten(),
+  # layers.Dense(128, activation='relu'),
+  # layers.Dense(num_classes)
+  tf.keras.layers.Flatten(input_shape=(img_height, img_width,3)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(3)
+])
+
+# compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+#Modal summary
+model.summary()
+
+# train the model
+epochs=10
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+# visualize training results:
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+
+#save tensorflow model history
+model.save('apm_model.h5')
+
+
+plt.show()
